d16221d250a0273337d67726ad294f101105783c
Abdur <karimovaleks19@gmail.com>
2025-12-05 00:53:46 +0500

    feat: upgrades socials links

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import i18next from "./i18next.js";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/vite"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
